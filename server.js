b7776291ceecbc5d8d15b65930d076abb4387859
YvesJaques <ymoraisjaques@gmail.com>
2024-01-16 15:57:53 -0300

    Setup friends route

--- server.js
+++ server.js
@@ -4,9 +4,36 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('<ul><li>Hello World</li></ul>');
+const friends = [   
+    {
+        id: 0,
+        name: 'Albert Einstein'
+    },
+    {
+        id: 1,
+        name: 'Sir Isaac Newton'
+    },
+    {
+        id: 2,
+        name: 'Nikola Tesla'
+    }
+]
+
+app.get('/friends', (req, res) => {
+    res.json(friends);
 });
+
+app.get('/friends/:friendId', (req, res) => {
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId];
+    if (friend) {
+        res.status(200).json(friend);
+    } else {
+        res.status(404).json({
+            message: 'Friend not found'
+        })
+    }
+})
 
 app.get('/messages', (req, res) => {
     console.log('Updating messages...');
