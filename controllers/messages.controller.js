fe67699dd94eeaebaad070a7ac2db82fa403aafa
YvesJaques <ymoraisjaques@gmail.com>
2024-01-19 18:53:16 -0300

    Layouts and separation of concerns

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,8 +1,11 @@
 const path = require('path');
 
 function getMessages(req, res) {    
-    res.sendFile(path.join(__dirname,'..','public', 'images','skimountain.jgp'));
-    // res.sendFile('<ul><li>Hello Albert!</li></ul>')
+    res.render('messages', {
+        title: 'Messages to my friends!',
+        friend: 'Elon Musk'
+    });
+    // res.sendFile(path.join(__dirname,'..','public', 'images','skimountain.jgp'));
 }
 
 function postMessage(req, res) {
