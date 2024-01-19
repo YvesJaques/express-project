e09ff0aad89aa826029f9bd67e6e7fc8c0d9ddf4
YvesJaques <ymoraisjaques@gmail.com>
2024-01-19 18:39:17 -0300

    Serving file through html

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 function getMessages(req, res) {    
-    res.sendFile(path.join(__dirname,'..','public','skimountain.jgp'));
+    res.sendFile(path.join(__dirname,'..','public', 'images','skimountain.jgp'));
     // res.sendFile('<ul><li>Hello Albert!</li></ul>')
 }
 
